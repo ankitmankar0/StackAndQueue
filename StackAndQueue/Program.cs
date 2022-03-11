8cfda3c54875dfcbdd5add988824526e9c92971f
ankitmankar0 <ankitmankar012@gmail.com>
2022-03-11 22:52:48 +0530

    Stack peak and pop

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace StackAndQueue
-
+namespace StackQueue
 {
-   class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("-------------Stack Operations-------------");
             Console.WriteLine("***1.Push operation***");
+            Console.WriteLine("***2.Peak operation***");
             Console.WriteLine("Enter the option");
             int num = Convert.ToInt32(Console.ReadLine());
             Stack stack = new Stack();
@@ -20,11 +24,18 @@ namespace StackAndQueue
                     stack.Push(56);
                     stack.Display();
                     break;
+                case 2:
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    stack.Pop();
+                    stack.Display();
+                    break;
                 default:
                     Console.WriteLine("Enter a valid option");
                     break;
             }
-                Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
--- StackAndQueue/Stack.cs
+++ StackAndQueue/Stack.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StackAndQueue
+namespace StackQueue
 {
     class Stack
     {
@@ -19,6 +19,36 @@ namespace StackAndQueue
             }
             this.top = newNode;
             Console.WriteLine("New Node {0} is added ", newNode.data);
+        }
+        //Peek method gives the top most element in stack without removing the element.
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("No elements present in the stack");
+                return;
+            }
+            Console.WriteLine("The element after peeking is : " + this.top.data);
+        }
+        //Pop Mthod-Removes the top most element
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("No elements present in the stack");
+                return;
+            }
+            Peek();
+            this.top = this.top.next;
+        }
+        //Checking the any underflow or overflow occurs
+        public void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
         }
         public void Display()
         {
